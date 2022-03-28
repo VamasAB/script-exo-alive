dfece4d002f52d3c9c8b95183b61f0a6e260f06f
Henka-dev <58777312+Henka-dev@users.noreply.github.com>
2022-03-28 13:35:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# script-exo-alive
+ 
---
+++ exo_alive.py
@@ -0,0 +1,38 @@
+import subprocess
+
+def get_all_tasks():
+    tasks = subprocess.check_output('tasklist /v').decode('cp866', 'ignore').split("\r\n")
+
+    for task in tasks:
+        print(task)
+
+
+def get_task(name):
+    tasks = subprocess.check_output('tasklist /v').decode('cp866', 'ignore').split("\r\n")
+
+    _ = []
+
+    for task in tasks:
+        if name in task:
+            _.append(task)
+    return _
+
+
+def check_if_alive(task_name):
+    task_list = get_task(task_name)
+
+    count = len(task_list)
+
+    if count > 0:
+        print('Found {} tasks named {}'.format(count, task_name))
+        for task in task_list:
+            if 'Not Responding' in task:
+                print('Task: {} is Not responding'.format(task_name))
+            else:
+                print('Task: {} is Running or Unknown'.format(task_name))
+    else:
+        print('No task(s) found.')
+
+
+check_if_alive('firefox.exe')
+#get_all_tasks()
