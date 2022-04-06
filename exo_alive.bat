286b7f8e7f989aa7855abd80df414c8c959fb3ef
Henka-dev <58777312+Henka-dev@users.noreply.github.com>
2022-04-06 10:33:51 +0200

    Update

---
+++ exo_alive.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\projects\script-exo-alive\venv\Scripts & activate & cd /d C:\projects\script-exo-alive\ & python main.py"
--- main.py
+++ main.py
@@ -1,11 +1,73 @@
-import subprocess, logging, time, os
+import subprocess, logging, time, smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 logging.basicConfig(
     filename='logger.log',
-    filemode='w',
+    # filemode='w',
     format='%(asctime)s - %(message)s',
     level=logging.INFO
 )
+
+debug = env.int('DEBUG')
+
+#https://realpython.com/python-send-email/
+
+def mail_to():
+    recipiants = env.list('RECIPIANTS')
+    smtp_server = env('SMTP_SERVER')
+    sender_email = env('SENDER_EMAIL')
+    port = env('PORT') # For SSL
+    password = env('PASSWORD')
+    exo_server = env('EXO_SERVER')
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f'Automatisk omstart av {exo_server}'
+    message["From"] = sender_email
+    message["To"] = ", ".join(recipiants)
+
+    # Create the plain-text and HTML version of your message
+    text = """\
+    Omstart har skett av {exo_server}."""
+    html = """\
+    <html>
+        <body>
+            <p>Omstart har skett av {exo_server}.</p>
+        </body>
+    </html>
+    """
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    try:
+        server = smtplib.SMTP(smtp_server,port)
+        server.ehlo() # Can be omitted
+        server.starttls(context=context) # Secure the connection
+        server.ehlo() # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, recipiants, message.as_string().format(exo_server=exo_server))
+        logging.info('Sent e-mails responsible staff.')
+    except Exception as e:
+        # Print any error messages to stdout
+        logging.info('Failed to send e-mails.')
+        print(e)
+    finally:
+        server.quit() 
+
 
 def get_task_status(task_name):
     tasks = subprocess.check_output(f'tasklist /v /fi "IMAGENAME eq {task_name}"').decode('cp866', 'ignore').split("\r\n")
@@ -16,16 +78,17 @@ def get_task_status(task_name):
     
     return 0
 
+
 def exo_handle(start=False):
-    # start_uri = 'C:\Program Files\EXO\EXOop4\Eo4Run.Exe'
-    # stop_uri = 'C:\Program Files\EXO\EXOstop.exe'
+    start_uri = ['C:\Program Files\EXO\EXOop4\Eo4Run.Exe', 'C:\Windows\\notepad.exe']
+    stop_uri = ['C:\Program Files\EXO\EXOstop.exe', 'C:\Windows\\winhlp32.exe']
 
     name = 'Eo4Run' if start else 'EXOstop'
-    uri = 'C:\Windows\\notepad.exe' if start else 'C:\Windows\\notepad.exe'
+    uri = start_uri[debug] if start else stop_uri[debug]
 
     try:
-        logging.info(f'Running {name}.')
-        status = subprocess.run(uri, check=True)
+        status = subprocess.Popen(uri)
+        logging.info(f'Succesfully ran {name}.')
         return status
     except:
         logging.info(f'{name} failed to run.')
@@ -33,17 +96,43 @@ def exo_handle(start=False):
 
 
 if __name__ == '__main__':
+    logging.info('-----------------------------------------------------')
     logging.info('Checking for hung processes..')
-    task_name = 'Notepad.exe' #'Eo4Run.Exe' #
-    task = get_task_status(task_name)
+    task_name = ['Eo4Run.Exe', 'Notepad.exe']
+    task = get_task_status(task_name[debug])
 
     if task == 0:
-        logging.info(f'Failed to find {task_name}.')
-    elif task:
-        logging.info(f'Task {task_name} is Running or Unknown.')
+        logging.info(f'Failed to find any running {task_name[debug]}.')
+    elif task and debug == 0:
+        logging.info(f'Task {task_name[debug]} is Running or Unknown, no action required.')
     else:
-        logging.info(f'{task_name} is Not Responding, restarting services.')
-        exo_handle()
-        time.sleep(45)
-        logging.info(f'{task_name} has been stoped, starting service.')
-        exo_handle(True)        
+        logging.info(f'{task_name[debug]} is Not Responding, restarting services.')
+        
+        retry = 1
+        # Trying to run Exostop 5 times before exiting script        
+        while retry <= 5:
+            logging.info(f'Running EXOstop, try {retry} of 5.')
+            stop_status = exo_handle()
+            if stop_status == 0:
+                retry += 1
+            else:
+                break
+        
+        # Trying to run Eo4Run if EXOstop was successful and Exo is not still running.
+        if stop_status != 0:
+            retry = 1
+            while retry <= 5:
+                time.sleep(10)
+                logging.info(f'Starting service {task_name[debug]}, try {retry} of 5.')
+                start_status = get_task_status(task_name[debug])
+                if start_status == 0:
+                    task = exo_handle(True)
+                    if task != 0:
+                        mail_to()
+                        break
+                    else:
+                        retry += 1
+                else:
+                    retry += 1
+        else:
+            logging.info(f'Failed to stop EXO service, closing down.')
