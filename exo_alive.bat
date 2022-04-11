2bfb164fd40e76902bd9e0af986d0ad360e73262
Henka-dev <58777312+Henka-dev@users.noreply.github.com>
2022-04-11 09:47:58 +0200

    Update exo_alive.bat

--- exo_alive.bat
+++ exo_alive.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\projects\script-exo-alive\venv\Scripts & activate & cd /d C:\projects\script-exo-alive\ & python main.py"
+cmd /c "cd /d C:\projects\script-exo-alive\venv\Scripts & activate & cd /d C:\projects\script-exo-alive\ & python main.py"
