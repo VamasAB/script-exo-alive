f13543391c9e63cd81d54b4b01e793f040d26bb7
Henka-dev <58777312+Henka-dev@users.noreply.github.com>
2022-04-20 11:28:48 +0200

    Update click.py

--- click.py
+++ click.py
@@ -7,4 +7,4 @@ def do_click(x,y, click=False):
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
-do_click(450,510)
+do_click(250,330)
